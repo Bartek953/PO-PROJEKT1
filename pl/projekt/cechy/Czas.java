5b77ee9297b96615b3bc9d12168b2b9c8a3c192d
Bartek953 <bartosz.tarnowski.953@gmail.com>
2026-05-02 16:53:33 +0200

    Implementacja klasy Czas

---
+++ pl/projekt/cechy/Czas.java
@@ -0,0 +1,80 @@
+package pl.projekt.cechy;
+
+import javax.management.RuntimeErrorException;
+
+public class Czas {
+    private int godzina;
+    private int minuta;
+    private int sekunda;
+
+    public Czas(int godzina, int minuta, int sekunda){
+        if (godzina < 0 || minuta < 0 || sekunda < 0){
+            throw new RuntimeException("Niepoprawny format czasu");
+        }
+        minuta += sekunda / 60;
+        this.sekunda = sekunda % 60;
+
+        godzina += minuta / 60;
+        this.minuta = minuta % 60;
+
+        if (godzina > 23){
+            throw new RuntimeException("Niepoprawny format czasu");
+        }
+        this.godzina = godzina;
+    }
+
+    public Czas(Czas czas){
+        this(czas.godzina(), czas.minuta(), czas.sekunda());
+    }
+
+    public int godzina(){
+        return godzina;
+    }
+    public int minuta(){
+        return minuta;
+    }
+    public int sekunda(){
+        return sekunda;
+    }
+
+    // Podaje czas w sekundach od 00:00:00
+    public int naSekundy(){
+        return sekunda() + 60 * (minuta() + 60 * godzina());
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder wynik = new StringBuilder("");
+
+        wynik.append(godzina());
+        wynik.append(":");
+
+        if (minuta() < 10){
+            wynik.append(0);
+        }
+        wynik.append(minuta());
+        wynik.append(":");
+
+        if (sekunda() < 10){
+            wynik.append(0);
+        }
+        wynik.append(sekunda());
+
+        return wynik.toString();
+    }
+
+    public static boolean rowny(Czas czas1, Czas czas2){
+        return czas1.naSekundy() == czas2.naSekundy();
+    }
+    public static boolean mniejszy(Czas czas1, Czas czas2){
+        return czas1.naSekundy() < czas2.naSekundy();
+    }
+    public static Czas dodaj(Czas czas1, Czas czas2){
+        return new Czas(
+                czas1.godzina() + czas2.godzina(),
+                czas1.minuta() + czas2.minuta(),
+                czas1.sekunda() + czas2.sekunda()
+        );
+    }
+
+}
